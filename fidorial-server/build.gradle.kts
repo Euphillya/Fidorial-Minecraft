b19581ff96d25522fce94e402cdfca18ef0db938
Toffikk <slodkitofik@gmail.com>
2026-09-10 19:29:01 +0200

    26.3 Release Candidate 1

--- fidorial-server/build.gradle.kts
+++ fidorial-server/build.gradle.kts
@@ -211,9 +211,9 @@ tasks.withType<GenerateItemPropertiesTask>().configureEach {
 
 fidorialRegistryGenerator {
     minecraftVersion.set(providers.gradleProperty("minecraftVersion"))
-    prismarineMinecraftData.set("26.3-snapshot-10")
+    prismarineMinecraftData.set("26.3-rc-1")
     prismarineDataRepository.set("Toffikk/minecraft-data") // PrismarineJS/minecraft-data
-    prismarineDataRef.set("ver/26.3-snapshot-10") // master
+    prismarineDataRef.set("ver/26.3-rc-1") // master
 
     generatedPackage.set(
         "fr.euphyllia.fidorial.server",
